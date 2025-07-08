676c967282a3d8bfb6794d98c78b5c68016dcea1
florian1345 <florian1345@gmx.de>
2025-07-08 21:45:31 +0200

    test skill value and extra SP and QL on success for partial state evaluation

--- model/tests/it/evaluate_partial.rs
+++ model/tests/it/evaluate_partial.rs
@@ -64,6 +64,157 @@ fn simple_call_with_zero_skill_points() {
 }
 
 #[test]
+fn simple_call_with_skill_points() {
+    // Spectacular Success: [1, 1, 1]
+    // => 1 / 8000 Value 20 (20 / 8000)
+    // Critical Success (QL 3): [1, 1, 2-10], [1, 2-12, 1], [2-14, 1, 1]
+    // => 33 / 8000 Value 6 (198 / 8000)
+    // Critical Success (QL 2): [1, 1, 11-13], [1, 13-15, 1], [15-17, 1, 1]
+    // => 9 / 8000 Value 4 (36 / 8000)
+    // Critical Success (QL 1): [1, 1, 14-20], [1, 16-20, 1], [18-20, 1, 1]
+    // => 15 / 8000 Value 2 (30 / 8000)
+    // Success (QL 3): [1-14, 1-12, 1-10] - Spectacular Success - Critical Success (QL 3)
+    // => 1646 / 8000 Value 3 (4938 / 8000)
+    // Success (QL 2):
+    //   ([1-14, 1-12, 11-13], [1-14, 13, 1-12], [1-14, 14, 1-11], [1-14, 15, 1-10],
+    //   [15, 1-12, 1-12], [15, 13, 1-11], [15, 14, 1-10],
+    //   [16, 1-12, 1-11], [16, 13, 1-10],
+    //   [17, 1-12, 1-10]) - Critical Success (QL 2)
+    // => 1384 / 8000 Value 2 (2768 / 8000)
+    // Success (QL 1):
+    //   ([1-14, 1-12, 14-17], [1-14, 13, 13-16], [1-14, 14, 12-15], [1-14, 15, 11-14],
+    //     [1-14, 16, 1-13], [1-14, 17, 1-12], [1-14, 18, 1-11], [1-14, 19, 1-10],
+    //   [15, 1-12, 13-16], [15, 13, 12-15], [15, 14, 11-14], [15, 15, 1-13], [15, 16, 1-12],
+    //     [15, 17, 1-11], [15, 18, 1-10],
+    //   [16, 1-12, 12-15], [16, 13, 11-14], [16, 14, 1-13], [16, 15, 1-12], [16, 16, 1-11],
+    //     [16, 17, 1-10],
+    //   [17, 1-12, 11-14], [17, 13, 1-13], [17, 14, 1-12], [17, 15, 1-11], [17, 16, 1-10],
+    //   [18, 1-12, 1-13], [18, 13, 1-12], [18, 14, 1-11], [18, 15, 1-10],
+    //   [19, 1-12, 1-12], [19, 13, 1-11], [19, 14, 1-10],
+    //   [20, 1-12, 1-11], [20, 13, 1-10]) - ([1, 1, 14-17], [1, 16-19, 1], [18-20, 1, 1])
+    // => 2263 / 8000 Value 1 (2263 / 8000)
+    // Spectacular Failure: [20, 20, 20]
+    // => 1 / 8000 Value -9 (-9 / 8000)
+    // Critical Failure: [1-19, 20, 20], [20, 1-19, 20], [20, 20, 1-19]
+    // => 57 / 8000 Value -7 (-399 / 8000)
+    // Failure: Everything else
+    // => 2591 / 8000 Value -1 (-2591 / 8000)
+
+    let engine = engine(
+        r#"
+        if is_spectacular_success then 20
+        else if is_critical_success then quality_level * 2
+        else if is_success then quality_level
+        else if is_spectacular_failure then -9
+        else if is_critical_failure then -7
+        else -1
+    "#,
+    );
+
+    let evaluated = engine
+        .evaluate_partial(PartialSkillCheckState {
+            attributes: [Attribute::new(14), Attribute::new(12), Attribute::new(10)],
+            skill_value: SkillPoints::new(7),
+            ..default()
+        })
+        .unwrap();
+
+    let expected_probs = SkillCheckOutcomeProbabilities::from([
+        outcome_prob(SpectacularFailure, 1.0 / 8000.0),
+        outcome_prob(CriticalFailure, 57.0 / 8000.0),
+        outcome_prob(Failure, 2591.0 / 8000.0),
+        outcome_prob(SpectacularSuccess(QL_3), 1.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_3), 33.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_2), 9.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_1), 15.0 / 8000.0),
+        outcome_prob(Success(QL_3), 1646.0 / 8000.0),
+        outcome_prob(Success(QL_2), 1384.0 / 8000.0),
+        outcome_prob(Success(QL_1), 2263.0 / 8000.0),
+    ]);
+    assert_that!(evaluated.evaluated).is_close_to(expected_probs, EPS);
+    assert_that!(evaluated.evaluation).is_close_to(eval(7254.0 / 8000.0), EPS);
+}
+
+#[test]
+fn extra_quality_level() {
+    // Probabilities are equal to `simple_call_with_skill_points`, with QL shifted up by 1.
+
+    let engine = engine("quality_level");
+
+    let evaluated = engine
+        .evaluate_partial(PartialSkillCheckState {
+            attributes: [Attribute::new(14), Attribute::new(12), Attribute::new(10)],
+            skill_value: SkillPoints::new(7),
+            extra_quality_levels_on_success: Some(QL_1),
+            ..default()
+        })
+        .unwrap();
+
+    let expected_probs = SkillCheckOutcomeProbabilities::from([
+        outcome_prob(SpectacularFailure, 1.0 / 8000.0),
+        outcome_prob(CriticalFailure, 57.0 / 8000.0),
+        outcome_prob(Failure, 2591.0 / 8000.0),
+        outcome_prob(SpectacularSuccess(QL_4), 1.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_4), 33.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_3), 9.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_2), 15.0 / 8000.0),
+        outcome_prob(Success(QL_4), 1646.0 / 8000.0),
+        outcome_prob(Success(QL_3), 1384.0 / 8000.0),
+        outcome_prob(Success(QL_2), 2263.0 / 8000.0),
+    ]);
+    assert_that!(evaluated.evaluated).is_close_to(expected_probs, EPS);
+    assert_that!(evaluated.evaluation).is_close_to(eval(15455.0 / 8000.0), EPS);
+}
+
+#[test]
+fn extra_skill_points_on_success() {
+    // Spectacular Success: [1, 1, 1]
+    // => 1 / 8000 Value 12 (12 / 8000)
+    // Critical Success (QL 2): [1, 1, 2-12], [1, 2-13, 1], [2-13, 1, 1]
+    // => 35 / 8000 Value 12 (420 / 8000)
+    // Critical Success (QL 1): [1, 1, 13-20], [1, 14-20, 1], [14-20, 1, 1]
+    // => 22 / 8000 Value 10 (220 / 8000)
+    // Success (QL 2):
+    //   [1-13, 1-13, 1-12] - Critical Success (QL 2) - Spectacular Success
+    // => 1992 / 8000 Value 12 (23904 / 8000)
+    // Success (QL 1):
+    //   ([1-13, 1-13, 13-14], [1-13, 14, 1-13], [1-13, 15, 1-12],
+    //   [14, 1-13, 1-13], [14, 14, 1-12],
+    //   [15, 1-13, 1-12]) - ([1, 1, 13-14], [1, 14-15, 1], [14-15, 1, 1])
+    // => 994 / 8000 Value 10 (9940 / 8000)
+    // Spectacular Failure: [20, 20, 20]
+    // => 1 / 8000 Value -5 (-5 / 8000)
+    // Critical Failure: [1-19, 20, 20], [20, 1-19, 20], [20, 20, 1-19]
+    // => 57 / 8000 Value -5 (-285 / 8000)
+    // Failure: Everything else
+    // => 4898 / 8000 Value -5 (-24490 / 8000)
+
+    let engine = engine("if is_success then 60.0 / (7.0 - as_float(quality_level)) else -5.0");
+
+    let evaluated = engine
+        .evaluate_partial(PartialSkillCheckState {
+            attributes: [Attribute::new(13), Attribute::new(13), Attribute::new(12)],
+            skill_value: SkillPoints::new(2),
+            extra_skill_points_on_success: SkillPoints::new(2),
+            ..default()
+        })
+        .unwrap();
+
+    let expected_probs = SkillCheckOutcomeProbabilities::from([
+        outcome_prob(SpectacularFailure, 1.0 / 8000.0),
+        outcome_prob(CriticalFailure, 57.0 / 8000.0),
+        outcome_prob(Failure, 4898.0 / 8000.0),
+        outcome_prob(SpectacularSuccess(QL_2), 1.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_2), 35.0 / 8000.0),
+        outcome_prob(CriticalSuccess(QL_1), 22.0 / 8000.0),
+        outcome_prob(Success(QL_2), 1992.0 / 8000.0),
+        outcome_prob(Success(QL_1), 994.0 / 8000.0),
+    ]);
+    assert_that!(evaluated.evaluated).is_close_to(expected_probs, EPS);
+    assert_that!(evaluated.evaluation).is_close_to(eval(9716.0 / 8000.0), EPS);
+}
+
+#[test]
 fn inaptitude_with_zero_skill_points() {
     // Attribute values 10, 10, 10 Skill value 0
     // Spectacular Success:
